6ad5d986eb5d062058d476e7e8763e599b01e119
Devin Sarao <devin.sarao@patelhs.org>
2020-10-15 14:38:05 +0000

    I added the if else statement part for "democrat"

--- Main.java
+++ Main.java
@@ -3,4 +3,7 @@ class Main {
   public static void main(String[] args) {
     //Start of Politcal Poll//
       System.out.println("Welcome to the offical Political Poll!"); Scanner scan = new Scanner(System.in);
-        System.out.println("Who is the political party you are affiliated to? "); 
+        System.out.println("Who is the political party you are affiliated to? "); //Political Party Question//
+        String pparty = scan.next();
+        if(pparty == "democrat"){ //If,Else Statement for political party//
+          
