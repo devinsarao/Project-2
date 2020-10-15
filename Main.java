abac486bb35e944d2927bbe6d10f7d200b9d4d48
Devin Sarao <devin.sarao@patelhs.org>
2020-10-15 14:33:22 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    //Start of Politcal Poll//
+      System.out.println("Welcome to the offical Political Poll!");
+ 
+        
