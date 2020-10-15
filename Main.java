912aa1beba32f88e94793768b5221d3bb5b06ef0
Devin Sarao <devin.sarao@patelhs.org>
2020-10-15 14:36:02 +0000

    I added a question with a scanner asking what politcal party they afficliate with.

--- Main.java
+++ Main.java
@@ -2,6 +2,5 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     //Start of Politcal Poll//
-      System.out.println("Welcome to the offical Political Poll!");
- 
-       
+      System.out.println("Welcome to the offical Political Poll!"); Scanner scan = new Scanner(System.in);
+        System.out.println("Who is the political party you are affiliated to? "); 
